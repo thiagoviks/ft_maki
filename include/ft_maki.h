e3e058ad94795d06670f2a4caf756cc9b0d38c5e
thiagoviks <thiagosudgomes@gmail.com>
2025-10-01 23:59:06 -0300

    added to-do strncpy function

--- include/ft_maki.h
+++ include/ft_maki.h
@@ -144,6 +144,7 @@ char				*ft_strchr(const char *s, int c);
 char				*ft_strrchr(const char *s, int c);
 char				*ft_strnstr(const char *big, const char *little, size_t len);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
+//need to implement strncpy
 
 int					ft_isalpha(int c);
 int					ft_isdigit(int c);
